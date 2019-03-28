a9578341ef1baa4d4d121d45a063c5d13dbdfacf
cheetosysst <32976729+cheetosysst@users.noreply.github.com>
2019-03-28 13:08:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import serial
 import io
+
+# Connect to serial port
 ser = serial.serial_for_url('/dev/ttyACM0', 115200, timeout=1)
 
+# if it's not on, open it
 if not ser.isOpen():
 	ser.open()
 
+# It's alive!!
 print("===Connection Start===\n", "Port: " ,ser.name)
 
+# I have no idea what the hell is it, well... it's there
 ser.flushInput()
 ser.flushOutput()
+
 
 try:
 	while True:
