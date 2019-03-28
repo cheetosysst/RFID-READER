8b6c35341f31ec1dce98c1981fa4443a7b5627fb
cheetosysst <32976729+cheetosysst@users.noreply.github.com>
2019-03-28 13:06:24 +0800

    comment translate

--- sketch_mar20a.ino
+++ sketch_mar20a.ino
@@ -1,13 +1,13 @@
 #include <SPI.h>
-#include <MFRC522.h>     // 引用程式庫
+#include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h>  // Arduino IDE 內建
+#include <Wire.h>  // Arduino IDE built in
 //#include <Keyboard.h>
-#define RST_PIN      A0        // 讀卡機的重置腳位
-#define SS_PIN       10        // 晶片選擇腳位
+#define RST_PIN      A0        // Reader pin
+#define SS_PIN       10
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // 建立MFRC522物件
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2C 位址
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // LCD I2C address
 
 void lcdinit() {
   
@@ -27,19 +27,20 @@ void setup() {
   Serial.begin(115200);
   lcdinit();
   SPI.begin();
-  mfrc522.PCD_Init();   // 初始化MFRC522讀卡機模組
+  mfrc522.PCD_Init();   // init mfrc522
   tone(2,600,300);
   //Keyboard.begin()
 }
 
 void loop() {
-    // 確認是否有新卡片
+    // check if there is a new card
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-      byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
-      byte idSize = mfrc522.uid.size;   // 取得UID的長度
+      byte *id = mfrc522.uid.uidByte;   // get UID
+      byte idSize = mfrc522.uid.size;   // get UID length
 
-      for (byte i = 0; i < idSize; i++) Serial.print(id[i],HEX);
+      for (byte i = 0; i < idSize; i++) Serial.print(id[i],HEX); // send to serial port
       
+      // Print on lcd
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("TYPE:");
@@ -48,10 +49,12 @@ void loop() {
       lcd.write("ID: ");
       tone(2,600,300);
       
-      for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
+      for (byte i = 0; i < idSize; i++) {  // show 
         lcd.print(id[i],HEX);
         if (i != idSize-1) lcd.write("-");
       }
-      mfrc522.PICC_HaltA();  // 讓卡片進入停止模式
+      
+      
+      mfrc522.PICC_HaltA();  // stop the reader
     } 
 }
